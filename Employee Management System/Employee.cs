3ea0255dabaef51ecd6de6464e0f286eedd24968
olfatnasser <89049666+olfatnasser@users.noreply.github.com>
2023-01-25 20:56:00 +0200

    update in function add in employee

--- Employee Management System/Employee.cs
+++ Employee Management System/Employee.cs
@@ -94,7 +94,7 @@ namespace Employee_Management_System
                     Con.SetData(Query);
                     ShowEmp();
                     MessageBox.Show("Employee Added!!!");
-                    EmpNameTb.Text = "";
+                  //  EmpNameTb.Text = "";
                     DailySalTb.Text = "";
                     GenCb.SelectedIndex = -1;
                     DepCb.SelectedIndex = -1;
