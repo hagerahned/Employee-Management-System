d32c684836ffde85187beb9cb64c6481a26e00d3
olfatnasser <89049666+olfatnasser@users.noreply.github.com>
2023-01-25 19:37:01 +0200

    update in design

--- Employee Management System/login.Designer.cs
+++ Employee Management System/login.Designer.cs
@@ -51,11 +51,11 @@ namespace Employee_Management_System
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(345, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 24);
+            this.label1.Size = new System.Drawing.Size(187, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Employee M S";
             // 
